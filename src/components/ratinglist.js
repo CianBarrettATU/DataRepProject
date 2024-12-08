59a5d933272e6ee0bb248c4dd996d689747651a4
CianBarrettATU <G00416157@atu.ie>
2024-12-08 16:25:27 +0000

    Rating Card styling

--- src/components/ratinglist.js
+++ src/components/ratinglist.js
@@ -1,29 +1,36 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
-import Games from "./games";
 
 const RatingList = () => {
     const [games, setGames] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         axios.get("http://localhost:4000/api/games")
-        .then((res)=> {
-            setGames(res.data.games);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    },[]);
+            .then((res) => {
+                setGames(res.data.games);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-    return(
-        <Card>
-            <Card.Body>
-                {games.map((game)=> (
-                    <p>{game.title}</p>
-                ))}
-            </Card.Body>
-        </Card>
+    return (
+        <div>
+            {games.map((game) => (
+                <Card key={game._id} style={{marginBottom: '20px', border: '2px solid black'}}>
+                    <Card.Body style={{display: 'flex', alignItems: 'center'}}>
+                        <img src={game.poster} style={{borderRadius: '5px', border: '3px solid black'}}/>
+                        <div>
+                        <h2>{game.title}</h2>
+                        <h2>Score: {game.rating || "no rating"} / 10</h2>
+
+                        </div>
+                    </Card.Body>
+                </Card>
+            ))}
+        </div>
+
     )
 }
 
